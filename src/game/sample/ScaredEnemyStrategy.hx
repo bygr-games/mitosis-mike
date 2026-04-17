d5f9502e8f8f0141fed097cad2461b895cc096d1
colorrin <mb.andres@gmail.com>
2026-04-17 08:39:41 +0200

    Scared Enemies Climb Small blocks

--- src/game/sample/ScaredEnemyStrategy.hx
+++ src/game/sample/ScaredEnemyStrategy.hx
@@ -2,6 +2,8 @@ package sample;
 
 class ScaredEnemyStrategy implements EnemyStrategy {
 	static inline var DETECTION_RANGE_TILES = 5.0;
+	static var STEP_HEIGHT_PX = Const.GRID;
+	static inline var COLLISION_EPSILON = 0.001;
 
 	var runSpeed = 0.055;
 
@@ -23,6 +25,9 @@ class ScaredEnemyStrategy implements EnemyStrategy {
 
 		var fleeDir = player.centerX >= enemy.centerX ? -1 : 1;
 		enemy.dir = fleeDir;
+
+		if( isOnGround(enemy) && enemy.hasWallInDirectionIgnoringLevelBounds(fleeDir) && tryClimbStep(enemy, fleeDir) )
+			return;
 
 		var canLeaveLevel = enemy.willExitLevelHorizontally(fleeDir, runSpeed);
 		if( isOnGround(enemy) && !enemy.hasWallInDirectionIgnoringLevelBounds(fleeDir) && ( enemy.hasGroundAhead(fleeDir) || canLeaveLevel ) )
@@ -68,5 +73,62 @@ class ScaredEnemyStrategy implements EnemyStrategy {
 
 	private function isOnGround(enemy:SampleEnemy):Bool {
 		return !enemy.destroyed && enemy.vBase.dy == 0 && enemy.hasGroundSupport();
+	}
+
+	function tryClimbStep(enemy:SampleEnemy, dir:Int):Bool {
+		var targetAttachX = enemy.attachX + dir * runSpeed * Const.GRID;
+		var targetAttachY = enemy.attachY - STEP_HEIGHT_PX;
+
+		if( !isPlacementFreeAt(enemy, enemy.attachX, targetAttachY) )
+			return false;
+
+		if( !isPlacementFreeAt(enemy, targetAttachX, targetAttachY) )
+			return false;
+
+		if( !hasGroundSupportAt(enemy, targetAttachX, targetAttachY) )
+			return false;
+
+		enemy.setPosPixel(targetAttachX, targetAttachY);
+		enemy.vBase.clearY();
+		enemy.vBump.clearY();
+		enemy.vBase.addX(dir * runSpeed);
+		return true;
+	}
+
+	function isPlacementFreeAt(enemy:SampleEnemy, targetAttachX:Float, targetAttachY:Float):Bool {
+		var targetLeft = targetAttachX - enemy.pivotX * enemy.wid;
+		var targetRight = targetAttachX + (1-enemy.pivotX) * enemy.wid;
+		var targetTop = targetAttachY - enemy.pivotY * enemy.hei;
+		var targetBottom = targetAttachY + (1-enemy.pivotY) * enemy.hei;
+		var leftCx = pxToLevelCoord(targetLeft + COLLISION_EPSILON);
+		var rightCx = pxToLevelCoord(targetRight - COLLISION_EPSILON);
+		var topCy = pxToLevelCoord(targetTop + COLLISION_EPSILON);
+		var bottomCy = pxToLevelCoord(targetBottom - COLLISION_EPSILON);
+
+		for( probeCy in topCy...bottomCy+1 )
+			for( probeCx in leftCx...rightCx+1 )
+				if( enemy.level.hasWallCollision(probeCx, probeCy) )
+					return false;
+
+		return true;
+	}
+
+	function hasGroundSupportAt(enemy:SampleEnemy, targetAttachX:Float, targetAttachY:Float):Bool {
+		var targetLeft = targetAttachX - enemy.pivotX * enemy.wid;
+		var targetRight = targetAttachX + (1-enemy.pivotX) * enemy.wid;
+		var targetBottom = targetAttachY + (1-enemy.pivotY) * enemy.hei;
+		var leftCx = pxToLevelCoord(targetLeft + COLLISION_EPSILON);
+		var rightCx = pxToLevelCoord(targetRight - COLLISION_EPSILON);
+		var supportCy = pxToLevelCoord(targetBottom + COLLISION_EPSILON);
+
+		for( probeCx in leftCx...rightCx+1 )
+			if( enemy.level.hasCollision(probeCx, supportCy) )
+				return true;
+
+		return false;
+	}
+
+	inline function pxToLevelCoord(v:Float):Int {
+		return Std.int(Math.floor(v / Const.GRID));
 	}
 }
